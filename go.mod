d45a6d6321bd5a93c6a0b55ce205cebf797a5051
暗香依飘 <appleapp2017@outlook.com>
2024-11-05 14:33:50 +0800

    2

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/anxiangyipiao/go_scheduler
 
 go 1.23
 
-require github.com/go-co-op/gocron/v2 v2.12.3
-
-require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/jonboulle/clockwork v0.4.0 // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
-	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
-)
+require github.com/lizongying/cron v1.0.1-0.20231124021926-d68aae18ef13 // indirect
--- mian.go
+++ mian.go
@@ -1,73 +1,19 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"time"
-
-	"github.com/go-co-op/gocron/v2"
+	"github.com/lizongying/cron"
 )
 
-var _ gocron.Elector = (*myElector)(nil)
-
-type myElector struct {
-	num    int
-	leader bool
-}
-
-func (m myElector) IsLeader(_ context.Context) error {
-	if m.leader {
-		log.Printf("node %d is leader", m.num)
-		return nil
-	}
-	log.Printf("node %d is not leader", m.num)
-	return fmt.Errorf("not leader")
-}
-
 func main() {
-	
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-
-	for i := 0; i < 3; i++ {
-		go func(i int) {
-			elector := &myElector{
-				num: i,
-			}
-			if i == 0 {
-				elector.leader = true
-			}
-
-			scheduler, err := gocron.NewScheduler(
-				gocron.WithDistributedElector(elector),
-			)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			_, err = scheduler.NewJob(
-				gocron.DurationJob(time.Second),
-				gocron.NewTask(func() {
-					log.Println("run job")
-				}),
-			)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			scheduler.Start()
-
-			if i == 0 {
-				time.Sleep(5 * time.Second)
-				elector.leader = false
-			}
-			if i == 1 {
-				time.Sleep(5 * time.Second)
-				elector.leader = true
-			}
-		}(i)
-	}
-
-	select {} // wait forever
+	logger := cron.NewLoggerStdout()
+	c := cron.New(cron.WithStdout())
+	c.MustStart()
+	id := c.MustAddJob(new(cron.Job).
+		EverySecond(10).
+		MustSince("10:15").
+		Callback(func() {
+			logger.Info("callback")
+		}))
+	logger.Info("id", id)
+	select {}
 }
